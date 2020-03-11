59f390708e56af123c71491468a4e78079d49d45
Necryotiks <eman13vil@gmail.com>
2020-03-11 12:58:28 -0700

    Clarified parameters.
    Signed-off-by: Necryotiks <eman13vil@gmail.com>

---
+++ src/MEMORY/BRAM/single_port_bram.v
@@ -0,0 +1,9 @@
+module single_port_bram (
+	input wire i_CLK,
+	input wire i_WRITE_ENABLE,
+	input wire i_READ_ENABLE,
+	input wire i_ADDRESS,
+	input wire i_WRITE_DATA,
+	output reg o_READ_DATA
+);
+endmodule
--- src/MEMORY/ROM/generic_ROM.v
+++ src/MEMORY/ROM/generic_ROM.v
@@ -1,21 +1,21 @@
 
-module generic_ROM #(parameter p_ADDR_WIDTH = 4,parameter p_DATA_WIDTH = 8)(
+module generic_ROM #(parameter p_ADDRESS_WIDTH = 4,parameter p_DATA_WIDTH = 8)(
 	input wire i_CLK,
 	input wire i_READ_ENABLE,
-	input wire [p_ADDR_WIDTH-1:0] i_ADDRESS,
-	output reg [p_DATA_WIDTH-1:0] o_DATA
+	input wire [p_ADDRESS_WIDTH-1:0] i_ADDRESS,
+	output reg [p_DATA_WIDTH-1:0] o_READ_DATA
 );
 
-reg [p_DATA_WIDTH-1:0] r_ROM [0:2**p_ADDR_WIDTH-1];
+reg [p_DATA_WIDTH-1:0] r_ROM [0:2**p_ADDRESS_WIDTH-1];
 
 initial $readmemh("ROM.bin",r_ROM);
 
 always@(posedge i_CLK)
 begin
 	if(i_READ_ENABLE == 1'b1)
-		o_DATA <= r_ROM[i_ADDRESS];
+		o_READ_DATA <= r_ROM[i_ADDRESS];
 	else
-		o_DATA <= {p_DATA_WIDTH{1'b0}};
+		o_READ_DATA <= {p_DATA_WIDTH{1'b0}};
 
 end
 endmodule
