b432c69b5abee9e5c47648ab075a6125bb8a0bfc
Elliott Villars <elliottvillars@gmail.com>
2020-03-10 23:22:04 -0700

    Added a read enable to the ROM.
    Signed-off-by: Elliott Villars <elliottvillars@gmail.com>

--- src/MEMORY/ROM/generic_ROM.v
+++ src/MEMORY/ROM/generic_ROM.v
@@ -1,6 +1,7 @@
 
 module generic_ROM #(parameter p_ADDR_WIDTH = 4,parameter p_DATA_WIDTH = 8)(
 	input wire i_CLK,
+	input wire i_READ_ENABLE,
 	input wire [p_ADDR_WIDTH-1:0] i_ADDRESS,
 	output reg [p_DATA_WIDTH-1:0] o_DATA
 );
@@ -11,6 +12,10 @@ initial $readmemh("ROM.bin",r_ROM);
 
 always@(posedge i_CLK)
 begin
-	o_DATA <= r_ROM[i_ADDRESS];
+	if(i_READ_ENABLE == 1'b1)
+		o_DATA <= r_ROM[i_ADDRESS];
+	else
+		o_DATA <= {p_DATA_WIDTH{1'b0}};
+
 end
 endmodule
