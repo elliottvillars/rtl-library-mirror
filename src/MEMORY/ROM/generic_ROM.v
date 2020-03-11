35977537b76811046cf662e6fbc6e10a9f429a2b
Elliott Villars <elliottvillars@gmail.com>
2020-03-10 23:12:53 -0700

    Created a generic ROM.
    Signed-off-by: Elliott Villars <elliottvillars@gmail.com>

---
+++ src/MEMORY/ROM/generic_ROM.v
@@ -0,0 +1,16 @@
+
+module generic_ROM #(parameter p_ADDR_WIDTH = 4,parameter p_DATA_WIDTH = 8)(
+	input wire i_CLK,
+	input wire [p_ADDR_WIDTH-1:0] i_ADDRESS,
+	output reg [p_DATA_WIDTH-1:0] o_DATA
+);
+
+reg [p_DATA_WIDTH-1:0] r_ROM [2**p_ADDR_WIDTH-1:0];
+
+initial $readmemh("ROM.bin",r_ROM);
+
+always@(posedge i_CLK)
+begin
+	o_DATA <= r_ROM[i_ADDRESS];
+end
+endmodule
