3f228196925ab2310dca71427c13e8a74a0224b5
Elliott Villars <elliottvillars@gmail.com>
2020-03-26 19:52:26 -0700

    Need to add auto mode to the CIC compiler.
    Signed-off-by: Elliott Villars <elliottvillars@gmail.com>

--- src/DSP/FILTERS/CIC/CIC_FILTER_COMPILER/src/main.c
+++ src/DSP/FILTERS/CIC/CIC_FILTER_COMPILER/src/main.c
@@ -25,10 +25,9 @@
 //
 
 
-//TODO: enable custom directory
 //TODO: automatically generate parameters based on desired resolution. (i.e ENOB)
-//
-char * comm_list[] = {"--help","--bit_width","--integrator_stages","--comb_stages","--decimation_factor","--interpolator","--differential_delay"};
+//TODO: set input
+char * comm_list[] = {"--help","--bit_width","--integrator_stages","--comb_stages","--decimation_factor","--interpolator","--differential_delay","--DIR","--auto"};
 char * help_str = "--help: Prints this menu.\n\n--bit_width: Set the bit width of the CIC filter.\n\n--integrator_stages: Set the number of integrator stages.\n\n--comb_stages: Set the number of comb stages.\n\n--decimation_factor: Set the decimation factor.\n\n--differential_delay: Set the differential delay, either 1 or 2.\n\n--interpolator: Create a CIC interpolator. Default is decimator.\n\n";
 int main(int argc, char ** argv)
 {
@@ -38,6 +37,8 @@ int main(int argc, char ** argv)
 	int decimation_factor = 64;
 	int bool_is_decimator = 1;
 	int differential_delay = 1; //
+	char target_directory[64];
+	int bool_is_auto = 0;
 	FILE * fh;
 
 
@@ -73,16 +74,30 @@ int main(int argc, char ** argv)
 		{
 			bool_is_decimator = 0;
 		}
-		else if(!strcmp(argv[i],comm_list[6]))
+		else if(!strcmp(argv[i],comm_list[6]))//differential_delay
 		{
 			differential_delay = atoi(argv[i+1]);
 			i++;
+		}
+		else if(!strcmp(argv[i],comm_list[7])) //target directory
+		{
+			strcpy(target_directory,argv[i+1]);
+			i++;
+		}
+		else if(!strcmp(argv[i],comm_list[8]))
+		{
+			bool_is_auto = 1;
 		}
 		else
 		{
 			printf("%s is not a recognized argument. Exiting\n",argv[i]); 
 			exit(-1);
 		}
+	}
+	if(bool_is_auto & bool_is_decimator == 1)
+	{
+		char * input_freq;
+		char * output_freq;
 	}
 	printf("Current settings:\n");
 	(bool_is_decimator == 1) ? printf("DECIMATOR\n") : printf("INTERPOLATOR\n");
@@ -91,19 +106,34 @@ int main(int argc, char ** argv)
 	printf("COMB STAGES: %d\n",comb_stages);
 	printf("DECIMATION FACTOR: %d\n",decimation_factor);
 	printf("DIFFERENTIAL DELAY: %d\n",differential_delay);
+	printf("TARGET DIRECTORY: %s\n",target_directory);
 
 
 	if(bool_is_decimator == 1)
 	{
 		if(bool_is_decimator == 1)
 		{
-			fh = fopen("../../DECIMATOR/cic_decimator.v", "w+");
+			char * fname = "/cic_decimator.v";
+			char * path = strcat(target_directory,fname);
+			fh = fopen(path, "w+");
 		}
 		else
 		{
-			fh = fopen("../build/cic_interpolator.v", "w+");
+			char * fname = "/cic_interpolator.v";
+			char * path = strcat(target_directory,fname);
+			fh = fopen(path, "w+");
 		}
 		fprintf(fh,"//THIS IS AN AUTO GENERATED FILE.\n");
+		if(bool_is_auto == 1)
+		{
+			//request input freq
+			//request output freq
+			//request resolution
+			//F = fin/fout
+			//output bit_width = ceil(log2(F))
+			//ENOB = log4(F)
+			//stages = ceil(resolution/ENOB)
+		}
 		buildCICBase(fh,bit_width,decimation_factor,bool_is_decimator,integrator_stages,comb_stages,differential_delay);
 		buildIntegrator(fh,integrator_stages);
 		buildDownsampler(fh,decimation_factor);
@@ -113,9 +143,10 @@ int main(int argc, char ** argv)
 	else
 	{
 		printf("UNIMPLEMENTED!\n");
-		//write comb
+		exit(-1);
+		buildComb(fh,integrator_stages,comb_stages,differential_delay);
 		//write upsampler
-		//write integrator
+		buildIntegrator(fh,integrator_stages);
 	}
 	fprintf(fh,"endmodule\n");
 	fclose(fh);
